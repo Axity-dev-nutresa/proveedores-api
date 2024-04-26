4b067eb44d17bb1d6212d4e6d77de031704fe7f0
jesteban39 <juanesqb28@gmail.com>
2024-04-25 22:53:14 -0500

    2 lambdas

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import envVars from '@config/envVars'
 import db from '@db'
-import api from '@src/api/routes'
+import apiRoutes from '@src/api/routes'
 import serviceRoutes from '@src/services/routes'
 import swaggerJson from '@src/swagger.json'
 import {upload} from '@src/utils/multer'
 import swaggerUi from 'swagger-ui-express'
 import server from '@src/server'
 
-server.use('/api', api)
+server.use('/api', apiRoutes)
 server.use('/service', upload.single('file'), serviceRoutes)
 server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerJson))
 
 db.open()
   .then(() => {
     return server.listen(envVars.port, () => {
-      console.log('Server abierto en puerto: ', envVars.port)
+      console.log('Server listening on port: ', envVars.port)
     })
   })
   .catch((error) => console.error(error))
